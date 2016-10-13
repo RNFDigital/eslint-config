260a767a210f99cc626434d9ff9387912100d28c
Dale Jefferson <dale@dalejefferson.com>
2016-10-13 22:37:49 +0100

    Initial React Native globals

--- examples/react-native/index.js
+++ examples/react-native/index.js
@@ -1,1 +1,15 @@
 /* @flow */
+
+if (__DEV__) {
+    alert('Hello World') // eslint-disable-line no-alert
+
+    setImmediate(() => 1)
+
+    setInterval(() => 1, 1000)
+
+    requestAnimationFrame(() => 1)
+
+    setTimeout(() => {
+        console.log('Hello') // eslint-disable-line no-console
+    }, 1000)
+}
