d25fa191d79a7a8d55eff7c19ab27f488403c5a9
Dale Jefferson <dale@dalejefferson.com>
2016-10-16 09:34:12 +0100

    Make sure we check that the lock file is updated
    Also that the peer dependanies are correct

---
+++ scripts/.eslintrc.js
@@ -0,0 +1,6 @@
+/* @flow */
+/* eslint-env commonjs */
+
+module.exports = {
+    'extends': ['../node.js'],
+}
---
+++ scripts/checkPeerDependencies.js
@@ -0,0 +1,19 @@
+/* @flow */
+
+const packageJSON = require('../package.json')
+
+const arePackagesEqual = (packageJSON) => (packageName) => {
+    const peerDependency = packageJSON.peerDependencies[packageName]
+    const devDependency = packageJSON.devDependencies[packageName]
+
+    return peerDependency === devDependency
+}
+
+const peerPackageNames = Object.keys(packageJSON.peerDependencies)
+const peerDependenciesMatch = peerPackageNames.every(arePackagesEqual(packageJSON))
+
+if (!peerDependenciesMatch) {
+    throw new Error('Failure: Make sure you upgrade the peerDependencies')
+}
+
+console.log('Success: peerDependencies match devDependencies') // eslint-disable-line no-console
