1f9c39d79b6fc18674ee2ddef84923ed1369f0a6
Dale Jefferson <dale@dalejefferson.com>
2016-10-17 20:05:12 +0100

    react examples

--- examples/react/index.js
+++ examples/react/index.js
@@ -1,1 +1,28 @@
 /* @flow */
+
+import React from 'react'
+
+const selfClosing = <div/>
+const multiLine = (
+    <div>
+        <span>Hello World</span>
+        <div>{selfClosing}</div>
+    </div>
+)
+
+const singleProp = <input onChange={() => 1}/>
+const multiProp = (
+    <input
+        onBlur={() => 2}
+        onChange={() => 1}
+    >
+        {multiLine}
+    </input>
+)
+
+const a = ( // eslint-disable-line no-unused-vars
+    <div>
+        {singleProp}
+        {multiProp}
+    </div>
+)
