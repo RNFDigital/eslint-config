06ff2095e0c3d15fbb89324336ae55f16ed942ee
Dale Jefferson <dale@dalejefferson.com>
2016-10-17 20:50:54 +0100

    Update React rules

--- examples/react/index.js
+++ examples/react/index.js
@@ -2,7 +2,7 @@
 
 import React from 'react'
 
-const selfClosing = <div/>
+const selfClosing = <div />
 const multiLine = (
     <div>
         <span>Hello World</span>
@@ -10,19 +10,28 @@ const multiLine = (
     </div>
 )
 
-const singleProp = <input onChange={() => 1}/>
+const onChange = () => 1
+
+const singleProp = <input onChange={onChange} />
 const multiProp = (
     <input
-        onBlur={() => 2}
-        onChange={() => 1}
+        onBlur={onChange}
+        onChange={onChange}
     >
         {multiLine}
     </input>
+)
+const multiPropSelfClosing = (
+    <input
+        onBlur={onChange}
+        onChange={onChange}
+    />
 )
 
 const a = ( // eslint-disable-line no-unused-vars
     <div>
         {singleProp}
         {multiProp}
+        {multiPropSelfClosing}
     </div>
 )
